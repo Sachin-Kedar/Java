bd70ef0702ae8e16d7a3ad8b83ec73a89953c806
Sachin kedar <kedarsachin1020@gmail.com>
2025-06-20 09:02:54 +0530

    Done

--- Core_Java/Arrays.java
+++ Core_Java/Arrays.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Arrays {
     public static void main (String Args[]){
-        int[] arr = {100,98,89,99,76};
+        int[] arr = {104,94,83,59,76,103,107};
         float[] arr1 = {88.8f,99.8f,78.9f,87.6f,56.7f,67.3f};
 
+        /* 
         // Find out number is present in array or not
         Scanner sc = new Scanner(System.in);
         System.out.println("Enetr a number you want to find in Array: ");
         
-        int ab = sc.nextInt() ;
+        int ab = sc.nextInt() ;s
         boolean isPresent = false;
         for(int a: arr){
             if(a == ab){
@@ -25,5 +26,19 @@ public class Arrays {
             System.out.println("Enetred number is not present ");
 
         }
+
+        */
+
+        int max = 0;
+
+        int [] arr2 = {1,2,3,1,4,5,6,7};
+        int x= arr2[0];
+        for(int abc: arr2){
+            if(abc<x){
+                System.out.println("Array is not sorted");
+                break;
+             }
+        }
+        System.out.println("Array is sorted");
     }
 }
